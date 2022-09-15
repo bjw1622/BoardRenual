119aac0d1445329e0bf446748f8bb43872b674fb
JaeWon Baek <bjw1622@gmail.com>
2022-09-15 14:37:06 +0900

    Add Connection.cs

---
+++ Models/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BoardRenual.Models
+{
+    public class Connection
+    {
+        protected SqlConnection con;
+        public void Conn()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["BoardRenual"].ToString();
+            con = new SqlConnection(constr);
+        }
+        public void ConOpen()
+        {
+            con.Open();
+
+        }
+        public void ConClose()
+        {
+            con.Dispose();
+        }
+    }
+}
