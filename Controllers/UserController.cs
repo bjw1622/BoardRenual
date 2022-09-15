af0f4be9b6f9854fceccbd3dffd49e3abf51d89e
JaeWon Baek <bjw1622@gmail.com>
2022-09-15 14:37:18 +0900

    Add UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardRenual.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
