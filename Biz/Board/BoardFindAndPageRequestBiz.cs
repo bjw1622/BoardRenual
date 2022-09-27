e075d59bee6d6475c19d6acc004a5aa6fc815278
JaeWon Baek <bjw1622@gmail.com>
2022-09-27 14:40:16 +0900

    find and paging

---
+++ Biz/Board/BoardFindAndPageRequestBiz.cs
@@ -0,0 +1,26 @@
+﻿using BoardRenual.Models;
+using BoardRenual.Models.Orginal.Page;
+using BoardRenual.Models.Request.Page;
+using BoardRenual.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoardRenual.Biz.Board
+{
+    public class BoardFindAndPageRequestBiz
+    {
+        public List<BoardModel> PageAndFindBoard(FindAndPageRequestModel findAndPageRequestModel)
+        {
+            BoardRepository boardRepository = new BoardRepository();
+            PageOriginalModel pageOriginalModel = new PageOriginalModel();
+            pageOriginalModel.PageNumber = findAndPageRequestModel.PageNumber;
+            pageOriginalModel.PageCount = findAndPageRequestModel.PageCount;
+            pageOriginalModel.Variable = findAndPageRequestModel.Variable;
+            pageOriginalModel.Input = findAndPageRequestModel.Input;
+            Connection connection = new Connection();
+            return (boardRepository.PageAndFindBoard(pageOriginalModel, connection));
+        }
+    }
+}
