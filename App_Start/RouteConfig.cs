2d99e1ebef347313679b545afdfc0e3a3245b760
JaeWon Baek <bjw1622@gmail.com>
2022-10-18 17:08:28 +0900

    .

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -15,12 +15,6 @@ namespace BoardRenual
                 "Board/Detail/{No}",
                 new { controller = "Board", action = "Detail" }
             );
-            // "Board/Delete/{No}"에 대한 Route
-            routes.MapRoute(
-                "Delete",
-                "Board/Delete/{No}",
-                new { controller = "Board", action = "Delete", No = UrlParameter.Optional }
-            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
