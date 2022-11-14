f61536d33bffcbd11950838535629efd58067cc2
JaeWon Baek <bjw1622@gmail.com>
2022-11-14 11:02:46 +0900

    Write, GetIndex Api 생성

---
+++ BoardRenual/Controllers/BoardApiController.cs
@@ -0,0 +1,16 @@
+﻿using BoardRenual.Biz.Board;
+using System.Web.Http;
+
+namespace BoardRenual.Controllers
+{
+    public class BoardApiController : ApiController
+    {
+        [Route("api/Sample/Test")]
+        [HttpGet]
+        public IHttpActionResult IndexBoardList()
+        {
+            var result = new BoardGetBoardListBiz().GetBoardList();
+            return Ok(result);
+        }
+    }
+}
